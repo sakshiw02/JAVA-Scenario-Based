b33acb340ba788e9336c522e097d834cf4bc3dbb
sakshiw02 <90848830+sakshiw02@users.noreply.github.com>
2023-10-18 22:17:39 +0530

    Add files via upload

---
+++ person/Person1.java
@@ -0,0 +1,17 @@
+package person;
+
+public class Person1 {
+
+	public static void main(String[] args) {
+		Person pn1 = new Person("Sakshi ","Mumbai",24);
+		pn1.display();
+		pn1.setAge(23);
+		pn1.setCity("Thane");
+		pn1.getCity();
+		pn1.getAge();
+		pn1.display();
+		
+		
+	}
+
+}
